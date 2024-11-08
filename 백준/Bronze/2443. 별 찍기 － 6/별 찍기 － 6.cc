51c0f66dcc67d34d0775a55e8954271d1a6172a8
sumin <kimsuminn618@gmail.com>
2024-11-08 18:01:04 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int N;
+	cin >> N;
+
+	for (int i = 2 * N - 1; i > 0; i-=2) {
+		for (int j = 1; j < (2 * N - i); j+=2) {
+			cout << " ";
+		}
+
+		for (int j = 0; j < i; j++) {
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
